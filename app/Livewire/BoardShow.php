3ab95d97b659a99754f58c985ba603147dcde7fb
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-02 14:08:08 +0200

    mounted the board

--- app/Livewire/BoardShow.php
+++ app/Livewire/BoardShow.php
@@ -2,11 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Board;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class BoardShow extends Component
 {
+    public Board $board;
+    public function mount(){
+        $this->authorize('show',$this->board);
+    }
     #[Layout('layouts.app')]
     public function render()
     {
--- app/Policies/BoardPolicy.php
+++ app/Policies/BoardPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Board;
 use App\Models\User;
 
 class BoardPolicy
@@ -12,5 +13,9 @@ class BoardPolicy
     public function __construct()
     {
         //
+    }
+
+    public function show(User $user, Board $board){
+        return $user->id == $board->user_id;
     }
 }
