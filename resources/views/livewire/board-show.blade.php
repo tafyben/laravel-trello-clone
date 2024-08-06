5ffd017a968c5fd0cf6d3879c9b0a9cdfe217900
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-06 12:23:44 +0200

    fixed header problem which was shwing that the gb was white

--- resources/views/livewire/board-show.blade.php
+++ resources/views/livewire/board-show.blade.php
@@ -1,16 +1,20 @@
 <div>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold bg-gray text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ $board->title }}
-
-            <div class="w-full p-6 overflow-x-scroll">
-                <div class="flex w-max space-x-6 h-[calc(theme('height.screen')-64px-73px-theme('padding.12'))]">
-                    @foreach(range(1, 10) as $column)
-                        <livewire:column />
-                    @endforeach
-                </div>
-
-            </div>
         </h2>
+
     </x-slot>
+
+    <div class="w-full p-6 overflow-x-scroll">
+        <div class="flex w-max space-x-6 h-[calc(theme('height.screen')-64px-73px-theme('padding.12'))]">
+            @foreach(range(1, 10) as $column)
+                <livewire:column />
+            @endforeach
+        </div>
+
+    </div>
+
+
+
 </div>
