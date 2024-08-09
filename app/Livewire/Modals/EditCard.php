67d3f7af63b66c5e90753e32e8f6284eeeff7e27
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-09 09:22:59 +0200

    show dynamic card title

--- app/Livewire/Modals/EditCard.php
+++ app/Livewire/Modals/EditCard.php
@@ -2,11 +2,13 @@
 
 namespace App\Livewire\Modals;
 
+use App\Models\Card;
 use Livewire\Component;
 use LivewireUI\Modal\ModalComponent;
 
 class EditCard extends ModalComponent
 {
+    public Card $card;
     public function render()
     {
         return view('livewire.modals.edit-card');
--- resources/views/livewire/modals/edit-card.blade.php
+++ resources/views/livewire/modals/edit-card.blade.php
@@ -1,3 +1,3 @@
 <div>
-    Edit card
+    {{$card->title}}
 </div>
