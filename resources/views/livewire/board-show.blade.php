afb4614fb67e1f32ca96f622da35b880fd91b2fd
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-09 20:34:26 +0200

    styled the archived cards

--- resources/views/livewire/board-show.blade.php
+++ resources/views/livewire/board-show.blade.php
@@ -72,9 +72,5 @@
                 </button>
             </div>
         </div>
-
     </div>
-
-
-
 </div>
--- resources/views/livewire/modals/card-archive.blade.php
+++ resources/views/livewire/modals/card-archive.blade.php
@@ -1,5 +1,18 @@
 <x-modal-wrapper title="Archived cards">
 
-    Card Archive
+    <div class="max-h-96 overflow-y-scroll space-y-2">
+        @forelse($cards as $card)
+            <div class="border border-gray-200 rounded-lg px-3 py-2 flex items-center justify-between">
+                <div>
+                    {{$card->title}}
+                </div>
+                <button class="text-sm text-gray-500">
+                    Put back
+                </button>
+            </div>
+        @empty
+            <p>You have no archived cards here</p>
+        @endforelse
+    </div>
 
 </x-modal-wrapper>
