3614b49c94641e8f80e4938f14a107bc4101b7f3
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-09 06:36:46 +0200

    moving cards - installed srtable package to move cards succesfully

--- app/Livewire/BoardShow.php
+++ app/Livewire/BoardShow.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Board;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -11,6 +12,13 @@ class BoardShow extends Component
     public Board $board;
     public function mount(){
         $this->authorize('show',$this->board);
+    }
+
+    public function sorted(array $items){
+        $order = collect($items)->pluck('value')->toArray();
+        \App\Models\Column::setNewOrder($order,1,'id', function (Builder $query){
+            $query->where('user_id', auth()->id());
+        });
     }
     #[Layout('layouts.app')]
     public function render()
