c6f53dbb9b6e3af98396a5b7dea7a2ce5e54c9fc
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-08-10 07:21:50 +0200

    created bord modal

---
+++ app/Livewire/Modals/CreateBoard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Modals;
+
+use Livewire\Component;
+
+class CreateBoard extends Component
+{
+    public function render()
+    {
+        return view('livewire.modals.create-board');
+    }
+}
